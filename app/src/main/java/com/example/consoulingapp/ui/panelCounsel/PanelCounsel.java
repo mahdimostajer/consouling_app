d5955ff2f685c054ed021fd8bf603531c41a0f48
mahdimostajer <mahdi.mostajeran.ce@gmail.com>
2022-07-28 11:14:47 +0430

    add profile tab and counseling panel

--- app/src/main/java/com/example/consoulingapp/ui/panelCounsel/PanelCounsel.java
+++ app/src/main/java/com/example/consoulingapp/ui/panelCounsel/PanelCounsel.java
@@ -1,8 +1,9 @@
-package com.example.consoulingapp.ui.panel;
+package com.example.consoulingapp.ui.panelCounsel;
 
 import android.os.Bundle;
 
 import com.example.consoulingapp.R;
+import com.example.consoulingapp.databinding.ActivityPanelCounselBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,24 +13,24 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 
-public class PanelActivity extends AppCompatActivity {
+public class PanelCounsel extends AppCompatActivity {
 
-    private com.example.consoulingapp.databinding.ActivityPanelBinding binding;
+    private ActivityPanelCounselBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = com.example.consoulingapp.databinding.ActivityPanelBinding.inflate(getLayoutInflater());
+        binding = ActivityPanelCounselBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home2, R.id.navigation_history, R.id.navigation_shop, R.id.navigation_profile)
+                R.id.navigation_home2, R.id.navigation_dashboard)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_panel);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_panel_counsel);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
